bfcb4fe33b885420d1e94b9a9c987475d62ed00c
cristopher ontiveros <christoph.alano@gmail.com>
2024-01-14 17:33:59 -0600

    added the environment flags for pipelines and stacks

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,21 +1,30 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+
+import { config } from 'dotenv';
+
 import { InfrastructureStack } from '../lib/infrastructure-stack';
+import { InfrastructurePipelineStack } from '../lib/infrastructure-pipeline-stack';
+
+config({ path: process.env.DOTENV_CONFIG_PATH });
 
 const app = new cdk.App();
-new InfrastructureStack(app, 'InfrastructureStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+if (['ONLY_DEV'].includes(process.env.CDK_MODE || '')) {
+  new InfrastructureStack(app, `InfrastructureStack-${process.env.NODE_ENV || ''}`, {
+    env: { region: 'us-east-1', account: process.env.CDK_DEFAULT_ACCOUNT },
+  });
+}
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+if (['ONLY_PROD'].includes(process.env.CDK_MODE || '')) {
+  new InfrastructureStack(app, `InfrastructureStack-${process.env.NODE_ENV || ''}`, {
+    env: { region: 'us-east-1', account: process.env.CDK_DEFAULT_ACCOUNT },
+  });
+}
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+if (['ONLY_PIPELINE'].includes(process.env.CDK_MODE || '')) {
+  new InfrastructurePipelineStack(app, 'InfrastructurePipelineStack', {
+    env: { region: 'us-east-1', account: process.env.CDK_DEFAULT_ACCOUNT },
+  });
+}
